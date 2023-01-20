2b72492b01eb18f6b550c999efccae751eb111a4
Julia Shikanova <juliashikanova@yandex.ru>
2023-01-20 22:51:21 +0600

    Separate Hero component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,52 +1,5 @@
 <script>
-	import { MailIcon } from 'svelte-feather-icons';
+	import { Hero } from '$components';
 </script>
 
-<div class="hero container">
-	<div class="hero__wrapper">
-		<p class="hero__greetings">
-			Hello!
-			<span class="hero__waving-hand" aria-label="waving hand">👋</span>
-		</p>
-		<h1 class="hero__title">
-			My name is Julie
-			<br />
-			and I am frontend developer
-		</h1>
-		<a class="button" href="mailto:juliashikanova@yandex.ru">
-			Get in touch
-			<MailIcon size="16" />
-		</a>
-	</div>
-</div>
-
-<style>
-	.hero {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		min-height: 480px;
-	}
-
-	.hero__wrapper {
-		display: flex;
-		flex-direction: column;
-		align-items: flex-start;
-	}
-
-	.hero__greetings {
-		color: var(--orange);
-		font-size: 1.8rem;
-		font-weight: 600;
-	}
-
-	.hero__waving-hand {
-		display: inline-block;
-		transform: scaleX(-1);
-	}
-
-	.hero__title {
-		margin-bottom: 48px;
-	}
-</style>
+<Hero />
