cb554f731b386557ec61f822a90a1e14b35cb738
Julia Shikanova <juliashikanova@yandex.ru>
2023-01-24 23:06:03 +0600

    style: Main page (#7)
    * Reorganize Footer
    
    * Add flex-wrap to Hero's links
    
    * Remove SocialLinks
    
    * Update deps
    
    * Update fonts
    
    * Change Footer text font-size

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
 export { Header } from './header';
 export { default as Footer } from './footer.svelte';
-export { default as SocialLinks } from './social-links.svelte';
 export { default as Hero } from './hero.svelte';
