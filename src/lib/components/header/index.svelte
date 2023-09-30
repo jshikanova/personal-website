a57f1fa2277333bfa57f67fd5707d475b8b69ff4
Julia Shikanova <juliashikanova@yandex.ru>
2023-09-30 23:59:45 +0600

    style: Add blur edges to Header

--- src/lib/components/header/index.svelte
+++ src/lib/components/header/index.svelte
@@ -4,39 +4,42 @@
 	export let activeRoute: string;
 </script>
 
-<header class="header sticky top-fluid-5 z-10 col-span-full overflow-hidden">
+<header class="header sticky top-fluid-5 z-10 col-span-full">
 	<div class="container">
 		<div class="header__wrapper">
-			<a class="logo" href="/" title="JS logo / Go to Home page">
-				<span class="logo__text inline-flex origin-bottom-left transition-transform duration-500">
-					JS
-				</span>
-			</a>
-			<nav>
-				<ul class="flex flex-wrap gap-8 ps-0">
-					<li class="inline-flex pb-0">
-						<a
-							class="link nav-link"
-							class:active={activeRoute === '/resume'}
-							href="/resume"
-							class:nav-link_active={activeRoute === '/resume'}
-						>
-							Resume
-						</a>
-					</li>
-					<li>
-						<a
-							class="link nav-link"
-							href="mailto:juliashikanova@yandex.ru"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							Get in touch
-						</a>
-					</li>
-				</ul>
-			</nav>
-			<ThemeToggle />
+			<div class="edge"></div>
+			<div class="header__inner-wrapper">
+				<a class="logo" href="/" title="JS logo / Go to Home page">
+					<span class="logo__text inline-flex origin-bottom-left transition-transform duration-500">
+						JS
+					</span>
+				</a>
+				<nav>
+					<ul class="flex flex-wrap gap-8 ps-0">
+						<li class="inline-flex pb-0">
+							<a
+								class="link nav-link"
+								class:active={activeRoute === '/resume'}
+								href="/resume"
+								class:nav-link_active={activeRoute === '/resume'}
+							>
+								Resume
+							</a>
+						</li>
+						<li>
+							<a
+								class="link nav-link"
+								href="mailto:juliashikanova@yandex.ru"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								Get in touch
+							</a>
+						</li>
+					</ul>
+				</nav>
+				<ThemeToggle />
+			</div>
 		</div>
 	</div>
 </header>
@@ -47,17 +50,41 @@
 	}
 
 	.header__wrapper {
-		@apply grid items-center justify-between;
-		@apply border px-fluid-6 py-fluid-4 text-sm dark:border-black-60;
-		@apply supports-[backdrop-filter:blur(4px)]:bg-transparent supports-[backdrop-filter:blur(4px)]:backdrop-blur-sm;
+		@apply relative overflow-hidden;
+		@apply border dark:border-black-60;
+
+		border-radius: theme(spacing.fluid-8);
+	}
+
+	.header__inner-wrapper {
+		@apply supports-[backdrop-filter:blur(0)]:bg-transparent supports-[backdrop-filter:blur(0)]:backdrop-blur-md;
+		@apply grid items-center justify-between overflow-hidden;
+		@apply px-fluid-6 py-fluid-4 text-sm;
 
 		--column: minmax(80px, auto);
 
-		border-radius: theme(spacing.fluid-6);
 		grid-template-columns: repeat(2, var(--column));
 		@media screen(sm) {
 			grid-template-columns: var(--column) auto var(--column);
 		}
+	}
+
+	.edge::before,
+	.edge::after {
+		--offset: 2px;
+		--height: 100%;
+
+		@apply absolute inset-0 content-[''];
+		@apply h-[--height];
+
+		backdrop-filter: blur(24px) saturate(150%) brightness(1.2);
+	}
+	.edge::before {
+		top: calc(-1 * var(--height) + var(--offset));
+	}
+
+	.edge::after {
+		top: calc(100% - var(--offset));
 	}
 
 	nav {
